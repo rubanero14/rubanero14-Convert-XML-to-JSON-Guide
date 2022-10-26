35a0385bc4584c8f8325473519df9eb01dbe1814
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-26 10:41:59 +0800

    added cpress e2e testing

---
+++ cypress/e2e/firsttest.cy.js
@@ -0,0 +1,27 @@
+describe("example to-do app", () => {
+  it("Visit the page", () => {
+    cy.visit("https://rubanero14.github.io/RSS-Feed-CP-Prototype-Vue3/");
+  });
+
+  it("Open first Source", () => {
+    cy.wait(5000);
+    cy.get("img.logo[src='bw-logo.png'")
+      .should("be.visible")
+      .click({ multiple: true });
+  });
+
+  it("Open Second Source", () => {
+    cy.wait(5000);
+    cy.get("span.text-secondary.title")
+      .contains("Dividend News")
+      .should("be.visible")
+      .click({ multiple: true });
+  });
+
+  it("Back to Home Page", () => {
+    cy.get("button.btn-secondary.w-100")
+      .contains("Back")
+      .should("be.visible")
+      .click();
+  });
+});
