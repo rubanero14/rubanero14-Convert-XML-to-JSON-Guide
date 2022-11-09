5a7558ae33b44786671cb8eae5913a2be99399b4
Rajaruban Rajindram <ahyat888@gmail.com>
2022-11-09 19:07:15 +0800

    added desktop article layout version

--- src/components/ArticlesTiles.vue
+++ src/components/ArticlesTiles.vue
@@ -8,7 +8,7 @@
         <!-- Using the declared index and assign it to dynamic variable for CSS transition use -->
         <center :style="{ '--j': this.index }">
             <a :data-cy="`actions-article-${index}`" class="title" :href="feed.link.toString()" target="_blank">
-                <card-component class="mb-4">
+                <card-component class="mb-4" v-if="screenWidth <= 768">
                     <div class="row" v-if="titlePic(index, feeds, data)">
                         <div class="col-12">
                             <img class="banner" :src="titlePic(index, feeds, data)">
@@ -44,6 +44,47 @@
                             </div>
                         </div>
                     </div>
+                </card-component>
+                <card-component class="mb-4" v-else>
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div v-if="titlePic(index, feeds, data)">
+                            <img class="desktop" :src="titlePic(index, feeds, data)">
+                        </div>
+                        <div class="h-100 p-3">
+                            <div class="row">
+                                <div class="col-12">
+                                    <h3 class="title text-center text-secondary mb-0" v-if="screenWidth >= 1200">
+                                        {{ feed.title.toString().substr(0, 800).replace(': ','') + '...' }}
+                                    </h3>
+                                    <h3 class="title text-center text-secondary mb-0" v-else-if="screenWidth >= 600 && screenWidth < 1200">
+                                        {{ feed.title.toString().substr(0, 400).replace(': ','') + '...' }}
+                                    </h3>
+                                    <h3 class="title text-center text-secondary mb-0"  v-else>
+                                        {{ feed.title.toString().substr(0, 200).replace(': ','') + '...' }}
+                                    </h3>
+                                </div>
+                                <div class="col-12" v-if="articleDescription(index, feeds, data)">
+                                    <hr/>
+                                    <em>
+                                        <p class="description text-secondary mb-0" v-if="screenWidth >= 1200">
+                                            {{articleDescription(index, feeds, data).toString().substr(0, 800).replace(': ','') + '...'}}<span class="text-primary">read more</span>
+                                        </p>
+                                        <p class="description text-secondary mb-0" v-else-if="screenWidth >= 600 && screenWidth < 1200">
+                                            {{articleDescription(index, feeds, data).toString().substr(0, 400).replace(': ','') + '...'}}<span class="text-primary">read more</span>
+                                        </p>
+                                        <p class="description text-secondary mb-0" v-else>
+                                            {{articleDescription(index, feeds, data).toString().substr(0, 200).replace(': ','') + '...' }}<span class="text-primary">read more</span>
+                                        </p>
+                                    </em>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="my-0"/>
+                    <div class="d-flex justify-content-between align-content-end p-3">
+                        <aside v-if="date()" class="time d-flex align-self-end text-secondary mb-0"><em>Updated: {{ date(index) }} ago</em></aside>
+                        <img class="img" v-if="pic" :src="pic" onerror="this.src='https://rss.com/favicon.ico'"/>
+                    </div>        
                 </card-component>
             </a>
         </center>
@@ -82,8 +123,15 @@ p.description{
 }
 
 img.banner {
-    height:  auto;
+    height: auto;
     width: 100%;
+    border-radius: 0;
+    margin: 0;
+}
+
+img.desktop {
+    height: 250px;
+    width: 250px;
     border-radius: 0;
     margin: 0;
 }
